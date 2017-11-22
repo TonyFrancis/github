e768a56b2b2fa77b517c9e781a2f943a5b21b0ed
Tony Francis <gtony@suyati.com>
2017-11-22 10:50:31 +0530

    react-router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import { createStore } from 'redux'
 import './index.css';
 import App from './App';
 import github from './reducers/github';
 import registerServiceWorker from './registerServiceWorker';
 
+const Hello = ({}) => (
+  <div>
+    Hello World
+  </div>
+)
+
 const store = createStore(github);
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <span>
+        <Route exact path="/" component={App} />
+        <Route path="/:name" component={Hello} />
+      </span>
+    </Router>
   </Provider>, document.getElementById('root'));
 registerServiceWorker();
